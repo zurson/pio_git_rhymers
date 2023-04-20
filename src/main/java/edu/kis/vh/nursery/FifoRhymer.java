4f652438a76410d67514084fdd63a6093af8abbe
Verionn <bijacik.adam@gmail.com>
2023-04-20 11:35:30 +0200

    Ustawienie modyfikatoru final przy niemutowalnych atrybutach klas

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
