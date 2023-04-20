01a18ff1803f8a0e5b4526d254b64357d3724282
Verionn <bijacik.adam@gmail.com>
2023-04-20 11:26:19 +0200

    Zastapienie literalow stalymi

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int INITIAL_VALUE = -1;
+    public static final int MAX_NUMBERS = 12;
+    private final int[] numbers = new int[MAX_NUMBERS];
 
-    public int total = -1;
+    public int total = INITIAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == INITIAL_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_NUMBERS-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return INITIAL_VALUE ;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return INITIAL_VALUE;
         return numbers[total--];
     }
 
