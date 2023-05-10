286523373b0eeef531692e381c57d46aa426a82b
zurson <matu.jurek@gmail.com>
2023-05-10 21:13:08 +0200

    13. Uporzadkowano aplikacje demo RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -6,32 +6,60 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 class RhymersDemo {
 
     private static final int MAX_NUMBERS_IN_ROW = 15;
-    private static final int ROWS = 15;
+    private static final int ROWS = 3;
     private static final int MAX_RANDOM_NUMBER = 20;
 
     public static void main(String[] args) {
+
         RhymersFactory factory = new DefaultRhymersFactory();
-        
-        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
+        testRhymers(factory);
+
+    }
+
+    private static void testRhymers(RhymersFactory factory) {
+
+        DefaultCountingOutRhymer[] rhymers = rhymersInit(factory);
+        fillRhymersWithKnownData(rhymers);
+        fillRhymersWithRandomData(rhymers);
+
+        printResult(rhymers);
+
+    }
+
+    private static DefaultCountingOutRhymer[] rhymersInit(RhymersFactory factory){
+
+        return new DefaultCountingOutRhymer[]{factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
+
+    }
+
+    private static void fillRhymersWithKnownData(DefaultCountingOutRhymer[] rhymers){
+
         for (int i = 1; i < MAX_NUMBERS_IN_ROW; i++)
             for (int j = 0; j < ROWS; j++)
                 rhymers[j].countIn(i);
-        
+
+    }
+
+    private static void fillRhymersWithRandomData(DefaultCountingOutRhymer[] rhymers){
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < MAX_NUMBERS_IN_ROW; i++)
             rhymers[3].countIn(rn.nextInt(MAX_RANDOM_NUMBER));
-        
+
+    }
+
+    private static void printResult(DefaultCountingOutRhymer[] rhymers){
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
+
     }
-    
+
 }
