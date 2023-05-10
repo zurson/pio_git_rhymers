d9b8c0a6ccfc1e118a2c06159279f730938bcd48
zurson <matu.jurek@gmail.com>
2023-05-10 21:45:08 +0200

    17. Wygenerowanie dokumentacji oraz jej uzupelnienie

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,17 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * The object that manages the linked list
+ */
 public class IntLinkedList {
 
     private static final int LIST_IS_EMPTY = -1;
     private Node last;
 
+    /**
+     * Adds new value to the list
+     * @param i Value you want to add to list
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -15,16 +22,28 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Checks if the list is empty
+     * @return Returns true if list is empty and false otherwise
+     */
     private boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Returns top value
+     * @return Returns top value
+     */
     public int top() {
         if (isEmpty())
             return LIST_IS_EMPTY;
         return last.getValue();
     }
 
+    /**
+     * Returns and deletes last item of the list
+     * @return Retuns last element of the list
+     */
     public int pop() {
         if (isEmpty())
             return LIST_IS_EMPTY;
