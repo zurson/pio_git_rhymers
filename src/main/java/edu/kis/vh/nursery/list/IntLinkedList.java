e04155ca7447f901746dc930438d86566242f92f
zurson <matu.jurek@gmail.com>
2023-05-10 21:16:44 +0200

    14. *Usuniecie publicznej klasy Node i przeniesienie jej do klasy IntLinekList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -34,3 +34,34 @@ public class IntLinkedList {
     }
 
 }
+
+class Node {
+
+    private final int value;
+    private Node prev;
+    private Node next;
+
+    public Node(int i) {
+        value = i;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
